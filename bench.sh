6382716241a16f7fbf24e6c72a32c587d37b559c
Peter Mattis <petermattis@gmail.com>
2024-02-16 05:15:04 -0500

    swiss: add benchmarks from github.com/zhangyunhao116/gomapbench
    These benchmarks cover a wider variety of scenarios than what was previously
    being tested.

--- bench.sh
+++ bench.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-go test -v -run - -bench . -count 10 -timeout 1h | tee out
-grep -v swissMap out | sed 's,runtimeMap,Map,g' > out.runtime
-grep -v runtimeMap out | sed 's,swissMap,Map,g' > out.swiss
+go test -c
+./swiss.test -test.v -test.run - -test.bench . -test.count 10 -test.benchmem -test.timeout 10h | tee out
+grep -v swissMap out | sed 's,/runtimeMap,,g' > out.runtime
+grep -v runtimeMap out | sed 's,/swissMap,,g' > out.swiss
 benchstat out.runtime out.swiss
